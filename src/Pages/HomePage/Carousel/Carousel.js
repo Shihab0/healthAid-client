a5180e430dfde9db51011cee24a6e130cf2a1fb7
shihab0 <ataharshihab1@gmail.com>
2022-11-10 20:05:34 +0600

    corousel text added

--- src/Pages/HomePage/Carousel/Carousel.js
+++ src/Pages/HomePage/Carousel/Carousel.js
@@ -22,7 +22,7 @@ const Carousel = () => {
       >
         <SwiperSlide className="bg bg1 text-center">
           <div className="text-slate-300">
-            <h2 className="text-6xl font-bold font-mono mt-32">
+            <h2 className="text-4xl md:text-6xl font-bold font-mono mt-32">
               Welcome To Health Aid
             </h2>
             <p className="mt-4">
@@ -35,9 +35,51 @@ const Carousel = () => {
             </button>
           </div>
         </SwiperSlide>
-        <SwiperSlide className="bg bg2">Slide 2</SwiperSlide>
-        <SwiperSlide className="bg bg3">Slide 3</SwiperSlide>
-        <SwiperSlide className="bg bg4">Slide 4</SwiperSlide>
+        <SwiperSlide className="bg bg2">
+          <div className="text-slate-300 text-center">
+            <h2 className="text-4xl md:text-6xl font-bold font-mono mt-32">
+              All doctors treat, but a good doctor lets nature heal.
+            </h2>
+            <p className="mt-4">
+              No matter how your financial situation is, <br /> we treat you for
+              free. Receive free treatment from renowned doctors anytime,
+              anywhere from any device.{" "}
+            </p>
+            <button className="btn btn-wide mt-4 btn-warning btn-outline">
+              Explore More
+            </button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="bg bg3">
+          <div className="text-slate-300 text-center">
+            <h2 className="text-4xl md:text-6xl font-bold font-mono mt-32">
+              Enjoy Your Life — We'll Be Here to Save it!
+            </h2>
+            <p className="mt-4">
+              No matter how your financial situation is, <br /> we treat you for
+              free. Receive free treatment from renowned doctors anytime,
+              anywhere from any device.{" "}
+            </p>
+            <button className="btn btn-wide mt-4 btn-warning btn-outline">
+              Explore More
+            </button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="bg bg4">
+          <div className="text-slate-300 text-center">
+            <h2 className="text-4xl md:text-6xl font-bold font-mono mt-32">
+              All doctors treat, but a good doctor lets nature heal.
+            </h2>
+            <p className="mt-4">
+              No matter how your financial situation is, <br /> we treat you for
+              free. Receive free treatment from renowned doctors anytime,
+              anywhere from any device.{" "}
+            </p>
+            <button className="btn btn-wide mt-4 btn-warning btn-outline">
+              Explore More
+            </button>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
